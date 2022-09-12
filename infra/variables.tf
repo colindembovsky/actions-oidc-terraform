f4cece6c02733f597cf8957414df26cef27b58e9
Colin Dembovsky <colindembovsky@gmail.com>
2022-09-12 07:14:16 +0000

    Linting

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,9 +1,11 @@
 variable "resource_group_location" {
   default     = "southcentralus"
   description = "Location of the resource group."
+  type        = string
 }
 
 variable "resource_group_name" {
   default = "cd-teroidc"
   description = "Name of the resource group."
+  type        = string
 }
